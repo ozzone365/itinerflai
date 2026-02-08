acb9d3426cc13201691b0b5062daa966254d3e0f
ozzone365 <ozzone365@gmail.com>
2026-02-08 11:56:08 +0200

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    supabaseUrl: process.env.S_URL,
-    supabaseKey: process.env.S_KEY,
-    openaiKey: process.env.O_KEY
+  res.setHeader('Content-Type', 'application/json');
+  return res.status(200).json({
+    supabaseUrl: process.env.S_URL || "",
+    supabaseKey: process.env.S_KEY || "",
+    openaiKey: process.env.O_KEY || ""
   });
 }
